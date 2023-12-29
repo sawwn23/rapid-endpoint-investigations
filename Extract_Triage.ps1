c0c711484bdf7ff7cf799d71f0b4c27302cb2b03
sawwinnnaung <saw.wn@kernellix.com>
2023-12-29 23:29:59 +0630

    changes to my box

---
+++ Extract_Triage.ps1
@@ -0,0 +1,10 @@
+﻿﻿# Must use PowerShell 7.x to avoid long path/filename output issues
+# Change the three variables below to match your file/folder paths
+$casename = 'snwlab'
+$compressed_triage_data = "C:\w\RapidIR\$casename\triage_data"
+$unzipped_triage_data = "C:\w\RapidIR\$casename\triage_data"
+
+(get-childitem -path $compressed_triage_data -filter *.zip).basename | ForEach-Object {
+    
+    Expand-Archive -path $compressed_triage_data\$_.zip -DestinationPath $unzipped_triage_data\$_ -Force
+}
